6f1c2d3dda94494acd5c3a4eb76790e3b54cf303
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-11-08 09:42:54 +0400

    Create DataPersistance to save / load game status

---
+++ Assets/Scripts/Model/GameRules.cs
@@ -0,0 +1,14 @@
+
+namespace CardMatchingGame.Model
+{
+    public static class GameRules
+    {
+        public static int Rule
+        {
+            get => _rule;
+            set => _rule = value * 2;
+        }
+
+        private static int _rule;
+    }
+}
